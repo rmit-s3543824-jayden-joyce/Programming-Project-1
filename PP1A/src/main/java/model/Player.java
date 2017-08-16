0a9aa0fce065f24c56403ef9a1af02af86225941
rmit-s3542969-John-Nguyen <s3542969@student.rmit.edu.au>
2017-08-16 17:03:49 +1000

    Edit player profile

--- PP1A/src/main/java/model/Player.java
+++ PP1A/src/main/java/model/Player.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Player extends User{
 	
 	String user_ID;
@@ -9,14 +17,59 @@ public class Player extends User{
 		this.user_ID = user_ID;
 	}
 	
-	public Player editProfile(String user_ID, String user_fname, 
-			                  String user_lname, String password){	
+	//Players to edit their own personal details 
+	public Player editProfile(String oldId, String user_ID, 
+			                  String password, String user_fname, 
+			                  String user_lname, int age) throws IOException{	
+		FileTools ft = new FileTools();
+		
 		Player editedPlayer = null;
+		String oldName = ft.USER_DATA_FILE;
+		String tempName = "tmpFile.txt";
+		
+		//Read from database file, and write up a new tmpfile with update
+		try {
+			String line, newData;
+			//manipulating files
+			BufferedReader br = new BufferedReader(new FileReader(oldName));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(tempName));
+			
+			Player editPlayer;
+			while((line = br.readLine()) != null){
+				if(line.contains(oldId)){
+					//creating a new edited file
+					editPlayer = new Player(user_ID, password, user_fname, user_lname, age);
+					//edit player to CSV format
+					String strPlayer = ft.toCSV(editPlayer);
+					bw.write(strPlayer + "\r\n");
+					continue;
+				}
+				//writing unedited line
+				bw.write(line + "\r\n");
+			}
+			
+			br.close();
+			bw.close();
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		//deleting old file
+		File oldFile = new File(oldName);
+		oldFile.delete();
+		//renaming new edited file with the old name
+		File tempFile = new File(tempName);
+		tempFile.renameTo(oldFile);
+		
 		return editedPlayer;
 	}
 	
+	//Opening a new trading account with initial balance
 	public TradingAcc openTradeAcc(String user_ID){
 		TradingAcc newAcc = new TradingAcc(user_ID);
+		System.out.println("Successfully Open a Trading Account!");
+		newAcc.currBal = newAcc.INIT_BAL;
+		
 		return newAcc;
 	}
 	
