c65e3a3100cf385683ed1fb0483dfe5aab022d7d
Karsten Luu <lyp.karstenluu@outlook.com>
2017-08-18 02:12:04 +1000

    added functionality to read from JSON in url and save data into a file. Added some dependencies

--- PP1A/src/main/java/model/FileTools.java
+++ PP1A/src/main/java/model/FileTools.java
@@ -5,9 +5,95 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.json.JSONObject;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 public class FileTools {
 	public static final String USER_DATA_FILE = "src/main/resources/userData.csv";
+	public static final String ASX_COMPANIES_DATA_FILE = "src/main/resources/ASXListedCompanies.csv";
+    public static final String ASX_JSON_PATH = "data.asx.com.au/data/1/share/";
+    public static final String ASX_JSON_PAST_DAILY = "/prices?interval=daily&count=";
+	
+    //fetch data from a JSON in a url using its ASX code, if ASX code is not a key, then return null
+	public JSONObject fetchShareData(String ASXcode) throws IOException
+	{
+		JSONObject json = null;
+		URL url = new URL("http://" + ASX_JSON_PATH + ASXcode);
+		InputStream is;
+		BufferedReader br;
+		
+		if (!urlIsValid(url))
+		{
+			return null;
+		}
+		
+		is = url.openStream();
+		br = new BufferedReader(new InputStreamReader(is));
+		json = new JSONObject(br.readLine());	
+		
+		if (!json.has("open_price"))
+		{
+			return null;
+		}
+		
+		br.close();
+		
+		return json;
+	}
+	
+	//checks if url is valid
+	public boolean urlIsValid(URL url) throws IOException
+	{
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("HEAD");
+		return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
+	}
+	
+	//Separate function for writing changes to csv file after fetching data
+	public void overwriteCSV(List<String[]> companiesCSVlist, String filePath) throws IOException
+	{
+		CSVWriter csvWriter = new CSVWriter(new FileWriter(filePath));
+		csvWriter.writeAll(companiesCSVlist);
+		csvWriter.flush();
+		csvWriter.close();
+	}
+	
+	//fetch all data from from JSON url and save them to file
+	public void fetchAllShareData()
+	{
+		try {
+			JSONObject json = null;
+			List<String[]> companiesCSVlist;
+			//read from csv and add edit vals
+			CSVReader csvReader = new CSVReader(new FileReader(ASX_COMPANIES_DATA_FILE));
+			companiesCSVlist = csvReader.readAll();
+			
+			for (int i = 1; i < companiesCSVlist.size(); i++)
+			{
+				json = fetchShareData(companiesCSVlist.get(i)[1]);
+				if (json != null)
+				{
+					companiesCSVlist.get(i)[3] = json.get("open_price").toString();
+				}
+			}
+			
+			csvReader.close();
+			
+			//write to csv (rewrite everything)
+			overwriteCSV(companiesCSVlist, ASX_COMPANIES_DATA_FILE);
+		}
+		catch (IOException e){
+			
+		}
+	}
 
 	//use user ID to find player in a file and return it
 	public User LoadPlayer(String user_ID)
