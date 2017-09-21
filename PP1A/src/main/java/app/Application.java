2391554531eb39bde318fb5d5c32bb5cac670bc9
YiQuan <s3522101@student.rmit.edu.au>
2017-09-21 18:14:15 +1000

    Initialize leaderboard

--- PP1A/src/main/java/app/Application.java
+++ PP1A/src/main/java/app/Application.java
@@ -25,29 +25,21 @@ public class Application {
 		port(getHerokuAssignedPort());
 		menu = new Menu();
 		
-		get("/",                       controller.LoginController.mainPage);
-		get("/login",                  controller.LoginController.loginPage);
-		get("/redirectUser",           controller.LoginController.redirectUser);
-		get("/register",               controller.RegisterController.registerPage);
-		get("/regSuccess",             controller.RegisterController.regSuccess);
-		get("/user",                   controller.UserController.userPage);		
-		get("/EditProfile",            controller.UserController.editPage);
-		get("/ConfirmEditProfile",     controller.UserController.confirmEditProfile);
-		get("/admin",                  controller.AdminController.adminPage);
-		get("/TransactionAccount",     controller.TransactionController.transactionAccount);
+		get("/",						controller.LoginController.mainPage);
+		get("/login",					controller.LoginController.loginPage);
+		get("/redirectUser",			controller.LoginController.redirectUser);
+		get("/register",				controller.RegisterController.registerPage);
+		get("/regSuccess",				controller.RegisterController.regSuccess);
+		get("/user",					controller.UserController.userPage);		
+		get("/EditProfile",				controller.UserController.editPage);
+		get("/ConfirmEditProfile",		controller.UserController.confirmEditProfile);
+		get("/admin",					controller.AdminController.adminPage);
+		get("/TransactionAccount",		controller.TransactionController.transactionAccount);
 		get("/CompanyPage", 			controller.CompanyPageController.companyPage);
+		get("/Leaderboard", 			controller.LeaderboardController.leaderboard);
 		
 		//an example for example vtl
 		get("/example",                  controller.ExampleController.examplePage);
-		
-		//to test table
-		get("/testTable", (req, res) -> {
-			Map<String, Object> model = new HashMap<>();
-			
-			model.put("template", "/mainpage/testTable.vtl");
-			
-			return new VelocityTemplateEngine().render(new ModelAndView(model, "layout.vtl"));
-		});			
 	}
 	
 	static int getHerokuAssignedPort() {
